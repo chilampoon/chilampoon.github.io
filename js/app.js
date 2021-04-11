515268065075864cb5bdc10c65328e9538e5dc17
chilampoon <clpoon807@gmail.com>
2021-04-11 00:03:41 -0400

    Site updated: 2021-04-11 00:03:39

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// navbar burger
+$(document).ready(function() {
+
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function() {
+
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+
+  });
+});
+
+// preloader
+window.addEventListener('load',function(){
+  document.querySelector('body').classList.add("loaded")  
+});
